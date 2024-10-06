ae5202e76024ea0ab534012f2a6a790fc79fbb6f
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2024-10-06 17:28:48 +0200

    Add files via upload

---
+++ IterazioneEs10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main (int argc, char *argv[])
+{
+    int num;
+
+    do
+    {
+        printf("Inserire un numero intero: \n");
+        scanf("%d",&num);
+
+    } while (num < 0);
+    
+    int somma = 0;
+    int i = num;
+
+    do
+    {
+        somma = somma + i;
+        i = i-1;
+    } while (i > 0);
+    
+    printf("La somma è: %d", somma);
+
+
+    return 0;
+}
---
+++ IterazioneEs11.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+int main (int argc, char *argv[])
+{
+    int num;
+
+    do
+    {
+        printf("Inserire un numero intero: \n");
+        scanf("%d",&num);
+
+    } while (num < 0);
+    
+    int somma = 0;
+    int i = num; 
+
+    do
+    {
+        if((i - 1)%2 != 0)
+        {
+            somma = somma + i;
+            i = i-1;
+        }
+
+    } while (i > 0);
+    
+    printf("La somma è: %d", somma);
+
+
+    return 0;
+}
