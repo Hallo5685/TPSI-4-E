dd56a9645115f626c84685ce834495d06fe5e670
Hallo5685 <150442285+Hallo5685@users.noreply.github.com>
2024-12-01 20:38:05 +0100

    Completamento processiEs2.c

--- Processi/processiEs2.c
+++ Processi/processiEs2.c
@@ -18,9 +18,26 @@ int main()
 
     p1 = fork();
 
-    if(p1 > 0)
+    if(p1 == 0)
     {
-        
+        printf("Sono il processo figlio: %d mio padre è: %d", getpid(), getppid());
+        exit(0);
+    }
+
+    p2 = fork();
+
+    if(p2 == 0)
+    {
+        printf("Sono il processo figlio: %d mio padre è: %d", getpid(), getppid());
+        exit(0);
+    }
+
+    p3 = fork();
+
+    if(p3 == 0)
+    {
+        printf("Sono il processo figlio: %d mio padre è: %d", getpid(), getppid());
+        exit(0);
     }
 
     return 0;
